9aea49f99a3d6269bd48dda02ee6524ec573a53d
Isabel <isabelparejaescobar@gmail.com>
2026-01-07 13:50:09 +0100

    conexion modificada

--- config/db.js
+++ config/db.js
@@ -3,13 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = new Sequelize(
-    process.env.DB_NOMBRE,
-    process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.CONEXION,
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
         define: {
             timestamps: false
         },
